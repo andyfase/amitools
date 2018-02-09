01e6821f043acfa0e20214a8f2ff87a6d6873e0e
Andy <andrfase@a0999b11e339.ant.amazon.com>
2018-02-08 23:04:01 -0800

    init

---
+++ usr/lib64/ruby/site_ruby/ec2/amitools/defaults.rb
@@ -0,0 +1,13 @@
+# Copyright 2008-2014 Amazon.com, Inc. or its affiliates.  All Rights
+# Reserved.  Licensed under the Amazon Software License (the
+# "License").  You may not use this file except in compliance with the
+# License. A copy of the License is located at
+# http://aws.amazon.com/asl or in the "license" file accompanying this
+# file.  This file is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+# the License for the specific language governing permissions and
+# limitations under the License.
+
+EC2_X509_CERT = '/etc/ec2/amitools/cert-ec2.pem'
+EC2_X509_GOV_CERT = '/etc/ec2/amitools/cert-ec2-gov.pem'
+EC2_X509_CN_NORTH_1_CERT = '/etc/ec2/amitools/cert-ec2-cn-north-1.pem'
